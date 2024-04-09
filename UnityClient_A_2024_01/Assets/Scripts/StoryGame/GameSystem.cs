d8c56b74cc38e0720c10a567a278d9fecfa4797f
qwg12 <a_124@naver.com>
2024-04-09 10:31:19 +0900

    Feat:"GameSystem 스킵 업데이트"
    04_09

--- UnityClient_A_2024_01/Assets/Scripts/StoryGame/GameSystem.cs
+++ UnityClient_A_2024_01/Assets/Scripts/StoryGame/GameSystem.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;          //에디터
 using System.Text;          //텍스트 사용
+using UnityEngine.UI;
+using TMPro;
 
 namespace STORYGAME //이름충돌 방지
 {
@@ -21,6 +23,20 @@ namespace STORYGAME //이름충돌 방지
             {
                 gameSystem.ResetStoryModels();
             }
+
+            if (GUILayout.Button("Assing Text Component by Name"))
+            {
+                GameObject textObject = GameObject.Find("StroyTextUI");
+                if (textObject != null)
+                {
+                    Text textComponent = textObject.GetComponent<Text>();
+                    if (textComponent != null)
+                    {
+                        gameSystem.textComponent = textComponent;
+                        Debug.Log("Text Componet assinged Successfully");
+                    }
+                }
+            }
         }
     }
 #endif
@@ -28,13 +44,88 @@ namespace STORYGAME //이름충돌 방지
     public class GameSystem : MonoBehaviour
     {
         public static GameSystem instance;              //간단한 싱글톤 화
+        public Text textComponent = null;
+
+        public float delay = 0.1f;
+        public string fullText;
+        public string currentText = "";
+
+        public enum GAMESTATE
+        {
+            STORYSHOW,
+            WAITSELECT,
+            STROTYEND,
+            ENDMODE
+        }
+
+        public GAMESTATE currentState;
+        public StoryTableObject[] storyModels;
+        public StoryTableObject currentModels;
+        public int currentStoryIndex;
+        public bool showStroy = false;
 
         private void Awake()
         {
             instance = this;
+        }        
+
+        public void Start()
+        {
+            StartCoroutine(ShowText());
         }
 
-        public StoryTableObject[] storyModels;
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Q)) StoryShow(1);
+            if (Input.GetKeyDown(KeyCode.W)) StoryShow(2);
+            if (Input.GetKeyDown(KeyCode.E)) StoryShow(3);
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                delay = 0.0f;
+            }
+        }
+
+        public void StoryShow(int number)
+        {
+            if(!showStroy)
+            {
+                currentModels = FindStoryModel(number);
+                delay = 0.1f;
+                StartCoroutine(ShowText());
+            }            
+        }
+
+        StoryTableObject FindStoryModel(int number)
+        {
+            StoryTableObject tempStoryModels = null;
+            for(int i = 0; i < storyModels.Length; i++)
+            {
+                if(storyModels[i].storyNumeber == number)
+                {
+                    tempStoryModels = storyModels[i];
+                    break;
+                }
+            }
+
+            return tempStoryModels;
+
+        }
+
+        IEnumerator ShowText()
+        {
+            showStroy = true;
+            for(int i = 0; i <= currentModels.storyText.Length; i++)
+            {
+                currentText = currentModels.storyText.Substring(0, i);
+                textComponent.text = currentText;
+                yield return new WaitForSeconds(delay);
+            }
+
+            yield return new WaitForSeconds(delay);
+            showStroy = false;
+        }
+
 #if UNITY_EDITOR
         [ContextMenu("Reset Stroy Models")]
 
