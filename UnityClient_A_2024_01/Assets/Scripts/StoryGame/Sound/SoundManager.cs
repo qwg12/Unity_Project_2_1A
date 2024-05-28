bb91b62778d46d6d6cc0cd723474a0a78f6f7aaf
qwg12 <a_124@naver.com>
2024-05-28 10:40:32 +0900

    [Feat] Sound System 추가

---
+++ UnityClient_A_2024_01/Assets/Scripts/StoryGame/Sound/SoundManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume = 1f;
+
+    [Range(0f, 3f)]
+    public float pitch = 1f;
+    public bool loop;
+    public AudioMixerGroup mixerGroup;
+
+    [HideInInspector]
+    public AudioSource source;
+}
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    public List<Sound> sounds = new List<Sound>();
+    public AudioMixer audioMixer;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        foreach(Sound sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+            sound.source.loop = sound.loop;
+            sound.source.outputAudioMixerGroup = sound.mixerGroup;
+        }
+    }
+
+    public void PlaySound(string name)
+    {
+        Sound soundToPlay = sounds.Find(sound => sound.name == name);
+
+        if(soundToPlay != null)
+        {
+            soundToPlay.source.Play();
+        }
+        else
+        {
+            Debug.LogWarning("사운드" + name + "찾을 수 없습니다.");
+        }
+    }
+
+}
